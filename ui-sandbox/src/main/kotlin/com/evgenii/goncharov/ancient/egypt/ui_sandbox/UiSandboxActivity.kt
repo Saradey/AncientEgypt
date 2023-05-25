1389e4f87f9a9568871adf2cd966cead11180a93
Evgeny Goncharov <jtgn@yandex.ru>
2023-05-25 11:44:59 +0300

    added sandbox activity

---
+++ ui-sandbox/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/ui_sandbox/UiSandboxActivity.kt
@@ -0,0 +1,12 @@
+package com.evgenii.goncharov.ancient.egypt.ui_sandbox
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UiSandboxActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ui_sandbox)
+    }
+}
