d8f6ae7f148749d93184f5d359b86190886bbdea
Evgeny Goncharov <jtgn@yandex.ru>
2023-05-25 12:15:54 +0300

    refactor gradle file

---
+++ app/build.gradle.kts
@@ -0,0 +1,54 @@
+//TODO написать к релизу
+fun getVersionName(): String = "1.0"
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    namespace = "com.evgenii.goncharov.ancient.egypt"
+    compileSdk = rootProject.ext["compileSdkVersionApp"] as Int
+
+    defaultConfig {
+        applicationId = "com.evgenii.goncharov.ancient.egypt"
+        minSdk = rootProject.ext["minSdkVersionApp"] as Int
+        targetSdk = rootProject.ext["targetSdkVersionApp"] as Int
+        versionCode = rootProject.ext["versionCodeApp"] as Int
+        versionName = getVersionName()
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    android {
+        viewBinding.enable = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.5.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
