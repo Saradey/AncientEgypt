9419ea8dceec24590b3c7615df428bdea37fd723
Evgeny Goncharov <jtgn@yandex.ru>
2023-05-25 11:38:00 +0300

    refactor resources

--- ui-sandbox/build.gradle.kts
+++ ui-sandbox/build.gradle.kts
@@ -6,25 +6,13 @@ plugins {
 android {
     namespace = "com.evgenii.goncharov.ancient.egypt.ui_sandbox"
     compileSdk = 33
-
     defaultConfig {
         applicationId = "com.evgenii.goncharov.ancient.egypt.ui_sandbox"
         minSdk = 23
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -33,14 +21,19 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    android {
+        viewBinding.enable = true
+    }
 }
 
 dependencies {
+    implementation(coreProjectLibs.core.ktx)
+    implementation(uiLibs.app.compat)
+    implementation(uiLibs.material)
 
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(project(":ui-kit"))
+
+    testImplementation(testingLibs.junit)
+    androidTestImplementation(testingLibs.junit.androidx)
+    androidTestImplementation(testingLibs.espresso)
 }
