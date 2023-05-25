07bb1375db82b7f8f727657c3433d5e5a5d1dff6
Evgeny Goncharov <jtgn@yandex.ru>
2023-05-25 10:50:16 +0300

    refactor ui kit

--- ui-kit/build.gradle.kts
+++ ui-kit/build.gradle.kts
@@ -9,20 +9,7 @@ android {
 
     defaultConfig {
         minSdk = 23
-        targetSdk = 33
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
