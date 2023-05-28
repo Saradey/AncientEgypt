c162a8f57c45e795c33cbac1593407692ca73a6e
Evgeny Goncharov <jtgn@yandex.ru>
2023-05-28 15:06:08 +0300

    fix start destination

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/splash/SplashFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/splash/SplashFragment.kt
@@ -1,6 +1,7 @@
 package com.evgenii.goncharov.ancient.egypt.features.splash
 
 import androidx.fragment.app.Fragment
+import com.evgenii.goncharov.ancient.egypt.R
 
-class SplashFragment : Fragment() {
+class SplashFragment : Fragment(R.layout.fragment_splash) {
 }
