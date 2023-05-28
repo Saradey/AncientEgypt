53a218ecdb3a58287acea41c32fe353cc3aae947
Evgeny Goncharov <jtgn@yandex.ru>
2023-05-28 22:15:27 +0300

    added go to the main

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/map/SelectedArticleMapBottomSheetFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/map/SelectedArticleMapBottomSheetFragment.kt
@@ -2,5 +2,5 @@ package com.evgenii.goncharov.ancient.egypt.features.map
 
 import androidx.fragment.app.Fragment
 
-class SelectedArticleMapFragment : Fragment() {
+class SelectedArticleMapBottomSheetFragment : Fragment() {
 }
--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/splash/OnboardingFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/splash/OnboardingFragment.kt
@@ -1,7 +1,21 @@
 package com.evgenii.goncharov.ancient.egypt.features.splash
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.evgenii.goncharov.ancient.egypt.R
+import com.evgenii.goncharov.ancient.egypt.databinding.FragmentOnboardingBinding
 
 class OnboardingFragment : Fragment(R.layout.fragment_onboarding) {
+
+    private val vb: FragmentOnboardingBinding by viewBinding(FragmentOnboardingBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        vb.btnNext.setOnClickListener {
+            val navController = findNavController()
+            navController.navigate(OnboardingFragmentDirections.actionOnbaordingToMain())
+        }
+    }
 }
