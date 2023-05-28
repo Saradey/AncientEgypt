c64e29a87cc74a8f60987defa1d0c9bb5c6aa2e9
Evgeny Goncharov <jtgn@yandex.ru>
2023-05-28 18:30:03 +0300

    added stub view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,6 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/splash/SplashFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/splash/SplashFragment.kt
@@ -1,7 +1,19 @@
 package com.evgenii.goncharov.ancient.egypt.features.splash
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.evgenii.goncharov.ancient.egypt.R
+import com.evgenii.goncharov.ancient.egypt.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment(R.layout.fragment_splash) {
+
+    private val vb: FragmentSplashBinding by viewBinding(FragmentSplashBinding::bind)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        vb.btnNext.setOnClickListener {
+
+        }
+    }
 }
