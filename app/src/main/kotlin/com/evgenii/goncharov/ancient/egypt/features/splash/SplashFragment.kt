2f55acfaced3b4be8174a69e90f70d8efdd93fbc
Evgeny Goncharov <jtgn@yandex.ru>
2023-05-28 19:07:09 +0300

    added testing logic

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/splash/SplashFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/splash/SplashFragment.kt
@@ -1,7 +1,10 @@
 package com.evgenii.goncharov.ancient.egypt.features.splash
 
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.evgenii.goncharov.ancient.egypt.R
 import com.evgenii.goncharov.ancient.egypt.databinding.FragmentSplashBinding
@@ -10,10 +13,14 @@ class SplashFragment : Fragment(R.layout.fragment_splash) {
 
     private val vb: FragmentSplashBinding by viewBinding(FragmentSplashBinding::bind)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         vb.btnNext.setOnClickListener {
-
+            val navController = findNavController()
+            if (false) {
+                navController.navigate(SplashFragmentDirections.actionSplashToMain())
+            } else {
+                navController.navigate(SplashFragmentDirections.actionSplashToOnboarding())
+            }
         }
     }
 }
