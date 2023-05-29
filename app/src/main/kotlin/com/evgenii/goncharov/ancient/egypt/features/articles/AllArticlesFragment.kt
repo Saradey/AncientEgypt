5fd2f1395eb671c1b388b2cd6816c0e749a2d734
Evgeny Goncharov <jtgn@yandex.ru>
2023-05-29 09:40:37 +0300

    added layout all articles

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/articles/AllArticlesFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/articles/AllArticlesFragment.kt
@@ -1,6 +1,7 @@
 package com.evgenii.goncharov.ancient.egypt.features.articles
 
 import androidx.fragment.app.Fragment
+import com.evgenii.goncharov.ancient.egypt.R
 
-class AllArticlesFragment : Fragment() {
+class AllArticlesFragment : Fragment(R.layout.fragment_all_articles) {
 }
