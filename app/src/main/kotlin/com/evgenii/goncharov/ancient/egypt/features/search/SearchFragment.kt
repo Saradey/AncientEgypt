369213919fd74ae2809dfb6f7a0d4cef49c82994
Evgeny Goncharov <jtgn@yandex.ru>
2023-05-29 10:01:18 +0300

    added search layout

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/search/SearchFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/search/SearchFragment.kt
@@ -1,6 +1,7 @@
 package com.evgenii.goncharov.ancient.egypt.features.search
 
 import androidx.fragment.app.Fragment
+import com.evgenii.goncharov.ancient.egypt.R
 
-class SearchFragment : Fragment() {
+class SearchFragment : Fragment(R.layout.fragment_search) {
 }
