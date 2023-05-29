ce6341c7cbdecb4879a969b111e4a1e57fa58ca7
Evgeny Goncharov <jtgn@yandex.ru>
2023-05-29 13:56:57 +0300

    added containers and graphs

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/articles/FavoriteArticlesContainerFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/articles/FavoriteArticlesContainerFragment.kt
@@ -2,5 +2,5 @@ package com.evgenii.goncharov.ancient.egypt.features.articles
 
 import androidx.fragment.app.Fragment
 
-class FavoriteContainerFragment : Fragment() {
+class FavoriteArticlesContainerFragment : Fragment() {
 }
