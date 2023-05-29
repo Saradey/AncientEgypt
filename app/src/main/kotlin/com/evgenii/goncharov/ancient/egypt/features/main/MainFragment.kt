c327cad60486e5c057deced6438204d9633f8519
Evgeny Goncharov <jtgn@yandex.ru>
2023-05-29 09:34:37 +0300

    added layout main

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/MainFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/MainFragment.kt
@@ -1,6 +1,7 @@
 package com.evgenii.goncharov.ancient.egypt.features.main
 
 import androidx.fragment.app.Fragment
+import com.evgenii.goncharov.ancient.egypt.R
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.fragment_main) {
 }
