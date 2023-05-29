0224b9b1208832b8e8120c6cd4a4039f55213ede
Evgeny Goncharov <jtgn@yandex.ru>
2023-05-29 09:38:06 +0300

    added layout Favorite

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/articles/FavoriteArticlesFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/articles/FavoriteArticlesFragment.kt
@@ -1,6 +1,7 @@
 package com.evgenii.goncharov.ancient.egypt.features.articles
 
 import androidx.fragment.app.Fragment
+import com.evgenii.goncharov.ancient.egypt.R
 
-class FavoriteArticlesFragment : Fragment() {
+class FavoriteArticlesFragment : Fragment(R.layout.fragment_favorite_articles) {
 }
