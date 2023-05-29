9797ca9f2e1432b72189982cb84c9e3e7eeb478e
Evgeny Goncharov <jtgn@yandex.ru>
2023-05-29 09:35:53 +0300

    added layout settings

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/settings/SettingsFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/settings/SettingsFragment.kt
@@ -1,6 +1,7 @@
 package com.evgenii.goncharov.ancient.egypt.features.settings
 
 import androidx.fragment.app.Fragment
+import com.evgenii.goncharov.ancient.egypt.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
 }
