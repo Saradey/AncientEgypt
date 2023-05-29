b01c5e978fa7f718a7567e23ee6411e97c30ee6b
Evgeny Goncharov <jtgn@yandex.ru>
2023-05-29 11:39:00 +0300

    refactor

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/MainBottomMenuFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/MainBottomMenuFragment.kt
@@ -3,6 +3,9 @@ package com.evgenii.goncharov.ancient.egypt.features.main
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.evgenii.goncharov.ancient.egypt.R
 import com.evgenii.goncharov.ancient.egypt.databinding.FragmentMainBottomMenuBinding
