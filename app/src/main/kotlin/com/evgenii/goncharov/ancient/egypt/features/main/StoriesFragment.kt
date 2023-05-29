e5857761e204217b592eb806188b0962e54b0f8b
Evgeny Goncharov <jtgn@yandex.ru>
2023-05-29 14:05:38 +0300

    added stories and button to main

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/StoriesFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/StoriesFragment.kt
@@ -1,6 +1,7 @@
 package com.evgenii.goncharov.ancient.egypt.features.main
 
 import androidx.fragment.app.Fragment
+import com.evgenii.goncharov.ancient.egypt.R
 
-class StoriesFragment : Fragment() {
+class StoriesFragment : Fragment(R.layout.fragment_stories) {
 }
