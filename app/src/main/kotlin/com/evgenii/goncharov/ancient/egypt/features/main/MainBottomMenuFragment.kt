559cfa4cad74126e0bc244dae897e4b53ca153ff
Evgeny Goncharov <jtgn@yandex.ru>
2023-05-31 14:23:17 +0300

    fix

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/MainBottomMenuFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/MainBottomMenuFragment.kt
@@ -3,8 +3,10 @@ package com.evgenii.goncharov.ancient.egypt.features.main
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.navOptions
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.evgenii.goncharov.ancient.egypt.BottomMenuGraphDirections
 import com.evgenii.goncharov.ancient.egypt.R
@@ -17,6 +19,7 @@ class MainBottomMenuFragment : Fragment(R.layout.fragment_main_bottom_menu) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         vb.initUi()
+        initOnBackPressed()
     }
 
     private fun FragmentMainBottomMenuBinding.initUi() {
@@ -26,18 +29,27 @@ class MainBottomMenuFragment : Fragment(R.layout.fragment_main_bottom_menu) {
 
     private fun clickItemBottomMenu(item: MenuItem): Boolean {
         val navController = vb.fcvNestedBottomMenu.getFragment<NavHostFragment>().navController
+        val navOptions = navOptions {
+            launchSingleTop = true
+            restoreState = true
+            navController.currentDestination?.id?.let { id ->
+                popUpTo(id) {
+                    saveState = true
+                }
+            }
+        }
         when (item.itemId) {
             R.id.main_graph -> {
-                navController.navigate(BottomMenuGraphDirections.globalActionToMain())
+                navController.navigate(BottomMenuGraphDirections.globalActionToMain(), navOptions)
             }
             R.id.all_graph -> {
-                navController.navigate(BottomMenuGraphDirections.globalActionToAll())
+                navController.navigate(BottomMenuGraphDirections.globalActionToAll(), navOptions)
             }
             R.id.favourite_graph -> {
-                navController.navigate(BottomMenuGraphDirections.globalActionToFavourite())
+                navController.navigate(BottomMenuGraphDirections.globalActionToFavourite(), navOptions)
             }
             R.id.settings_graph -> {
-                navController.navigate(BottomMenuGraphDirections.globalActionToSettings())
+                navController.navigate(BottomMenuGraphDirections.globalActionToSettings(), navOptions)
             }
         }
         return true
@@ -47,4 +59,21 @@ class MainBottomMenuFragment : Fragment(R.layout.fragment_main_bottom_menu) {
         val navController = vb.fcvNestedBottomMenu.getFragment<NavHostFragment>().navController
         navController.navigate(BottomMenuGraphDirections.globalActionToSearch())
     }
+
+    private fun initOnBackPressed() {
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    val navController = vb.fcvNestedBottomMenu.getFragment<NavHostFragment>().navController
+                    navController.currentDestination?.id?.let { id ->
+                        if(!navController.popBackStack()) {
+                            requireActivity().finish()
+                        }
+                    }
+                }
+            }
+        )
+    }
+
 }
--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/MainFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/MainFragment.kt
@@ -1,8 +1,14 @@
 package com.evgenii.goncharov.ancient.egypt.features.main
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.evgenii.goncharov.ancient.egypt.R
 
 /** №2.2 */
 class MainFragment : Fragment(R.layout.fragment_main) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
 }
