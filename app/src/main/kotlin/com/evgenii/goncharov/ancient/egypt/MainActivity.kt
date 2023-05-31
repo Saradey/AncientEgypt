65b609bad7cf29142793bc2299620cfed93467ce
Evgeny Goncharov <jtgn@yandex.ru>
2023-05-31 15:28:55 +0300

    remove navigation from this project

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/MainActivity.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/MainActivity.kt
@@ -1,24 +1,12 @@
 package com.evgenii.goncharov.ancient.egypt
 
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.fragment.findNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setStartMainGraph()
-    }
-
-    //TODO this function replace start graph, if want show onboarding do nothing, if want show bottomMenu: replace graph
-    private fun setStartMainGraph() {
-        val navController =
-            supportFragmentManager.findFragmentById(R.id.fcv_root_container)?.findNavController()
-        if (false && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            navController?.setGraph(R.navigation.main_activity_graph)
-        }
     }
 }
--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/utils/extension/FragmentExt.kt
+++
@@ -1,9 +0,0 @@
-package com.evgenii.goncharov.ancient.egypt.utils.extension
-
-import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import com.evgenii.goncharov.ancient.egypt.R
-
-fun Fragment.findRootNavController(): NavController =
-    requireActivity().findNavController(R.id.fcv_root_container)
