b3c3e4420dbac28063813bd020d1c31a5dc14aef
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-01 15:47:15 +0300

    added logic BaseNavigator

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/base/BaseNavigator.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/base/BaseNavigator.kt
@@ -1,11 +1,44 @@
 package com.evgenii.goncharov.ancient.egypt.base
 
+import androidx.fragment.app.FragmentFactory
+import androidx.fragment.app.FragmentManager
 import com.github.terrakok.cicerone.Command
 import com.github.terrakok.cicerone.Navigator
 
-abstract class BaseNavigator : Navigator {
-
+abstract class BaseNavigator(
+    protected val fm: FragmentManager,
+    protected val ff: FragmentFactory,
+    protected val containerId: Int
+) : Navigator {
     override fun applyCommands(commands: Array<out Command>) {
+        fm.executePendingTransactions()
+        for (command in commands) {
+            try {
+                applyCommand(command)
+            } catch (e: RuntimeException) {
+                errorOnApplyCommand(e)
+            }
+        }
+    }
 
+    public abstract fun applyCommand(command: Command)
+
+    private fun errorOnApplyCommand(
+        error: RuntimeException
+    ) {
+        error(
+            ERROR_MESSAGE.format(
+                this.javaClass.canonicalName,
+                TAG,
+                error.message
+            )
+        )
+    }
+
+    private companion object {
+
+        const val TAG = "Navigator"
+        const val ERROR_MESSAGE =
+            "errorOnApplyCommand host: %s tag: %s error message: %s"
     }
 }
--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/navigation/MainActivityNavigator.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/navigation/MainActivityNavigator.kt
@@ -1,6 +1,10 @@
 package com.evgenii.goncharov.ancient.egypt.navigation
 
+import androidx.fragment.app.FragmentFactory
+import androidx.fragment.app.FragmentManager
 import com.evgenii.goncharov.ancient.egypt.base.BaseNavigator
 
-class MainActivityNavigator : BaseNavigator() {
+class MainActivityNavigator(
+
+) : BaseNavigator() {
 }
