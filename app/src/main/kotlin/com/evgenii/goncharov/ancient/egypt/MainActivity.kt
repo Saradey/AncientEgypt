0a59c9a829c3ee64c347c79f512972d26e8e424a
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-01 16:06:46 +0300

    added NavigationModule

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/MainActivity.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/MainActivity.kt
@@ -2,8 +2,17 @@ package com.evgenii.goncharov.ancient.egypt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.evgenii.goncharov.ancient.egypt.navigation.MainActivityNavigator
+import com.github.terrakok.cicerone.NavigatorHolder
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+//    @Inject
+//    lateinit var navigatorHolder: NavigatorHolder
+    private val navigator = MainActivityNavigator(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/di/NavigationModule.kt
@@ -0,0 +1,28 @@
+package com.evgenii.goncharov.ancient.egypt.di
+
+import com.github.terrakok.cicerone.Cicerone
+import com.github.terrakok.cicerone.NavigatorHolder
+import com.github.terrakok.cicerone.Router
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
+
+@Module
+@InstallIn(ActivityComponent::class)
+object NavigationModule {
+
+    @Provides
+    @ActivityScoped
+    fun provideCicerone(): Cicerone<Router> = Cicerone.create()
+
+    @Provides
+    @ActivityScoped
+    fun provideRouter(cicerone: Cicerone<Router>): Router = cicerone.router
+
+    @Provides
+    @ActivityScoped
+    fun provideNavigatorHolder(cicerone: Cicerone<Router>): NavigatorHolder =
+        cicerone.getNavigatorHolder()
+}
