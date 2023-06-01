9bc9b6efb01e142a62fd3ac8d1ca535b514e37c5
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-01 15:44:26 +0300

    added base class

---
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/base/BaseNavigator.kt
@@ -0,0 +1,11 @@
+package com.evgenii.goncharov.ancient.egypt.base
+
+import com.github.terrakok.cicerone.Command
+import com.github.terrakok.cicerone.Navigator
+
+abstract class BaseNavigator : Navigator {
+
+    override fun applyCommands(commands: Array<out Command>) {
+
+    }
+}
---
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/navigation/MainActivityNavigator.kt
@@ -0,0 +1,6 @@
+package com.evgenii.goncharov.ancient.egypt.navigation
+
+import com.evgenii.goncharov.ancient.egypt.base.BaseNavigator
+
+class MainActivityNavigator : BaseNavigator() {
+}
