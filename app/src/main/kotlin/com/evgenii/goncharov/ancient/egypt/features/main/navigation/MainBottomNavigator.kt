f2a9d4cf74f0f91a76bcafe5725ea5f67055e0fb
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-03 21:04:33 +0300

    added commit and forward

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/navigation/MainBottomNavigator.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/navigation/MainBottomNavigator.kt
@@ -1,11 +1,13 @@
 package com.evgenii.goncharov.ancient.egypt.features.main.navigation
 
+import androidx.fragment.app.commit
 import com.evgenii.goncharov.ancient.egypt.R
 import com.evgenii.goncharov.ancient.egypt.base.BaseNavigator
 import com.evgenii.goncharov.ancient.egypt.features.main.MainBottomMenuFragment
 import com.github.terrakok.cicerone.Back
 import com.github.terrakok.cicerone.Command
 import com.github.terrakok.cicerone.Forward
+import com.github.terrakok.cicerone.androidx.FragmentScreen
 import java.util.Stack
 
 class MainBottomNavigator(
@@ -27,9 +29,35 @@ class MainBottomNavigator(
     }
 
     private fun forward(command: Forward) {
-        val fragmentScreen = command.screen
+        val fragmentScreen = command.screen as FragmentScreen
         val backStackName = fragmentScreen.screenKey
+        if (selectedBackstackMenu != backStackName) {
+            commit(
+                fragmentScreen = fragmentScreen,
+                backStackName = backStackName
+            )
+            selectedBackstackMenu = backStackName
+            if (localBackStack.contains(selectedBackstackMenu)) {
+                localBackStack.remove(selectedBackstackMenu)
+            }
+            localBackStack.push(selectedBackstackMenu)
+        }
+    }
 
+    private fun commit(fragmentScreen: FragmentScreen, backStackName: String) {
+        if (selectedBackstackMenu.isNotEmpty()) {
+            fm.saveBackStack(selectedBackstackMenu)
+        }
+        if (!localBackStack.contains(backStackName)) {
+            val featureContainerFragment = fragmentScreen.createFragment(ff)
+            fm.commit {
+                setReorderingAllowed(true)
+                replace(containerId, featureContainerFragment, fragmentScreen.screenKey)
+                addToBackStack(backStackName)
+            }
+        } else {
+            fm.restoreBackStack(backStackName)
+        }
     }
 
     private fun back() {}
