9a0e2f0d4cbe0c8b308f548cdc7ba6e66578e35d
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-03 17:58:03 +0300

    added MainNavigationModule

---
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/di/MainNavigationModule.kt
@@ -0,0 +1,32 @@
+package com.evgenii.goncharov.ancient.egypt.features.main.di
+
+import com.github.terrakok.cicerone.Cicerone
+import com.github.terrakok.cicerone.NavigatorHolder
+import com.github.terrakok.cicerone.Router
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.FragmentComponent
+import dagger.hilt.android.scopes.FragmentScoped
+import javax.inject.Named
+
+@Module
+@InstallIn(FragmentComponent::class)
+object MainNavigationModule {
+
+    @Provides
+    @FragmentScoped
+    @Named("BottomMenu")
+    fun provideCicerone(): Cicerone<Router> = Cicerone.create()
+
+    @Provides
+    @FragmentScoped
+    @Named("BottomMenu")
+    fun provideRouter(cicerone: Cicerone<Router>): Router = cicerone.router
+
+    @Provides
+    @FragmentScoped
+    @Named("BottomMenu")
+    fun provideNavigatorHolder(cicerone: Cicerone<Router>): NavigatorHolder =
+        cicerone.getNavigatorHolder()
+}
