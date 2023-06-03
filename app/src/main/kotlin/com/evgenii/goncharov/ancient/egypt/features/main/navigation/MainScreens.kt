9cb9d2bddf0cbf7e2f775b6a36e993c27991bf3e
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-03 11:11:32 +0300

    refactor MainScreens

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/navigation/MainScreens.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/navigation/MainScreens.kt
@@ -1,11 +1,18 @@
 package com.evgenii.goncharov.ancient.egypt.features.main.navigation
 
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
 import com.evgenii.goncharov.ancient.egypt.features.main.MainBottomMenuFragment
 import com.github.terrakok.cicerone.androidx.FragmentScreen
 
 object MainScreens {
 
-    fun startMainBottomMenu() = FragmentScreen { _ ->
-        MainBottomMenuFragment.newInstance()
+    fun startMainBottomMenu() = object : FragmentScreen {
+        override val screenKey: String = BACKSTACK_NAME_BOTTOM_MENU
+        override fun createFragment(factory: FragmentFactory): Fragment {
+            return MainBottomMenuFragment.newInstance()
+        }
     }
+
+    const val BACKSTACK_NAME_BOTTOM_MENU = "bottomMenu"
 }
