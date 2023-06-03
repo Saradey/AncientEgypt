64779a21476f62b72d43bd378b7eecd8591e872c
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-03 20:48:07 +0300

    added empty forward and back

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/navigation/MainBottomNavigator.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/navigation/MainBottomNavigator.kt
@@ -3,7 +3,10 @@ package com.evgenii.goncharov.ancient.egypt.features.main.navigation
 import com.evgenii.goncharov.ancient.egypt.R
 import com.evgenii.goncharov.ancient.egypt.base.BaseNavigator
 import com.evgenii.goncharov.ancient.egypt.features.main.MainBottomMenuFragment
+import com.github.terrakok.cicerone.Back
 import com.github.terrakok.cicerone.Command
+import com.github.terrakok.cicerone.Forward
+import java.util.Stack
 
 class MainBottomNavigator(
     private val mainBottomMenuFragment: MainBottomMenuFragment
@@ -13,7 +16,17 @@ class MainBottomNavigator(
     containerId = R.id.fcv_nested_container_bottom_menu
 ) {
 
-    override fun applyCommand(command: Command) {
+    private val localBackStack: Stack<String> = Stack()
+    private var selectedBackstackMenu = ""
 
+    override fun applyCommand(command: Command) {
+        when (command) {
+            is Forward -> forward(command)
+            is Back -> back()
+        }
     }
+
+    private fun forward(command: Forward) {}
+
+    private fun back() {}
 }
