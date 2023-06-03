63f86bd7d9f176470d2df8f9bf8145b006f09f70
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-03 12:11:38 +0300

    added back stack name feature start

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/splash/navigation/SplashScreens.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/splash/navigation/SplashScreens.kt
@@ -9,16 +9,19 @@ import com.github.terrakok.cicerone.androidx.FragmentScreen
 object SplashScreens {
 
     fun startSplash() = object : FragmentScreen {
-        override val screenKey: String = ""
+        override val screenKey: String = BACKSTACK_NAME_SPLASH
         override fun createFragment(factory: FragmentFactory): Fragment {
             return SplashFragment.newInstance()
         }
     }
 
     fun startOnboarding() = object : FragmentScreen {
-        override val screenKey: String = ""
+        override val screenKey: String = BACKSTACK_NAME_ONBOARDING
         override fun createFragment(factory: FragmentFactory): Fragment {
             return OnboardingFragment.newInstance()
         }
     }
+
+    private const val BACKSTACK_NAME_ONBOARDING = "onboarding"
+    private const val BACKSTACK_NAME_SPLASH = "splash"
 }
