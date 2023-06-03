00886160571be69d5845525b355f6f9223883949
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-03 10:50:46 +0300

    added screenKey to SplashScreens

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/splash/navigation/SplashScreens.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/splash/navigation/SplashScreens.kt
@@ -1,16 +1,24 @@
 package com.evgenii.goncharov.ancient.egypt.features.splash.navigation
 
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
 import com.evgenii.goncharov.ancient.egypt.features.splash.OnboardingFragment
 import com.evgenii.goncharov.ancient.egypt.features.splash.SplashFragment
 import com.github.terrakok.cicerone.androidx.FragmentScreen
 
 object SplashScreens {
 
-    fun startSplash() = FragmentScreen { _ ->
-        SplashFragment.newInstance()
+    fun startSplash() = object : FragmentScreen {
+        override val screenKey: String = ""
+        override fun createFragment(factory: FragmentFactory): Fragment {
+            return SplashFragment.newInstance()
+        }
     }
 
-    fun startOnboarding() = FragmentScreen { _ ->
-        OnboardingFragment.newInstance()
+    fun startOnboarding() = object : FragmentScreen {
+        override val screenKey: String = ""
+        override fun createFragment(factory: FragmentFactory): Fragment {
+            return OnboardingFragment.newInstance()
+        }
     }
 }
