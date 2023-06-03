2e95030f89a8edaf5befaaa24d7ed6c4daf22664
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-03 11:08:26 +0300

    inject activityRouter to SplashFragment and added navigate

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/splash/SplashFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/splash/SplashFragment.kt
@@ -6,14 +6,27 @@ import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.evgenii.goncharov.ancient.egypt.R
 import com.evgenii.goncharov.ancient.egypt.databinding.FragmentSplashBinding
+import com.evgenii.goncharov.ancient.egypt.features.splash.navigation.SplashScreens
+import com.github.terrakok.cicerone.Router
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 /** №1.1 */
+@AndroidEntryPoint
 class SplashFragment : Fragment(R.layout.fragment_splash) {
+
+    @Inject lateinit var activityRouter: Router
 
     private val binding: FragmentSplashBinding by viewBinding(FragmentSplashBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.btnNext.setOnClickListener {
+            if (false) {
 
+            } else {
+                activityRouter.navigateTo(SplashScreens.startOnboarding())
+            }
+        }
     }
 
     companion object {
