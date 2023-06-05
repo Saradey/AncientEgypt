f0421f76b66bc81447f3dd3af315f82d2635e6b8
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-05 15:53:03 +0300

    hide bottom toolbar when open MainBottomMenuFragment

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/search/SearchFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/search/SearchFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.evgenii.goncharov.ancient.egypt.R
 import com.evgenii.goncharov.ancient.egypt.databinding.FragmentSearchBinding
+import com.evgenii.goncharov.ancient.egypt.features.main.contracts.SetVisibilityToBottomMenuToolbarListener
 
 /** №3 */
 class SearchFragment : Fragment(R.layout.fragment_search) {
@@ -13,7 +14,12 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
     private val binding: FragmentSearchBinding by viewBinding(FragmentSearchBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        (parentFragment as? SetVisibilityToBottomMenuToolbarListener)?.setVisibilityToolbar(false)
+    }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        (parentFragment as? SetVisibilityToBottomMenuToolbarListener)?.setVisibilityToolbar(true)
     }
 
     companion object {
