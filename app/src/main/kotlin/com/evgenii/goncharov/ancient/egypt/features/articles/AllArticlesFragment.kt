d03d99ecfe213a63de431884883baed3ea0dd15f
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-06 16:50:53 +0300

    added navigation AllArticlesFragment

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/articles/AllArticlesFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/articles/AllArticlesFragment.kt
@@ -6,14 +6,34 @@ import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.evgenii.goncharov.ancient.egypt.R
 import com.evgenii.goncharov.ancient.egypt.databinding.FragmentAllArticlesBinding
+import com.evgenii.goncharov.ancient.egypt.di.NavigationModule
+import com.evgenii.goncharov.ancient.egypt.features.articles.navigation.ArticlesScreens
+import com.evgenii.goncharov.ancient.egypt.features.contents.navigation.ContentScreens
+import com.github.terrakok.cicerone.Router
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import javax.inject.Named
 
 /** №4.1 */
+@AndroidEntryPoint
 class AllArticlesFragment : Fragment(R.layout.fragment_all_articles) {
 
+    @Inject @Named(NavigationModule.QUALIFIER_BOTTOM_MENU_NAVIGATION) lateinit var router: Router
+    @Inject @Named(NavigationModule.QUALIFIER_ACTIVITY_NAVIGATION) lateinit var mainActivityRouter: Router
     private val binding: FragmentAllArticlesBinding by viewBinding(FragmentAllArticlesBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.title.text = "${binding.title.text} ${toString()}"
+        binding.initUi()
+    }
+
+    private fun FragmentAllArticlesBinding.initUi() {
+        btnNext1.setOnClickListener {
+            router.navigateTo(ArticlesScreens.startSelectedCategory())
+        }
+        btnNext2.setOnClickListener {
+            mainActivityRouter.navigateTo(ContentScreens.startSelectedArticle())
+        }
     }
 
     companion object {
