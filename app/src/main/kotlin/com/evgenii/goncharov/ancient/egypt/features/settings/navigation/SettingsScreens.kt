b803d8756a907e1672f61a3bff8626fb741c3230
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-06 23:09:14 +0300

    refactor SettingsScreens

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/settings/navigation/SettingsScreens.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/settings/navigation/SettingsScreens.kt
@@ -1,17 +1,17 @@
 package com.evgenii.goncharov.ancient.egypt.features.settings.navigation
 
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentFactory
+import com.evgenii.goncharov.ancient.egypt.base.BaseFragmentScreen
 import com.evgenii.goncharov.ancient.egypt.features.main.navigation.MainBottomNavigator.Companion.BACKSTACK_NAME_SETTINGS
 import com.evgenii.goncharov.ancient.egypt.features.settings.SettingsFragment
-import com.github.terrakok.cicerone.androidx.FragmentScreen
 
 object SettingsScreens {
 
-    fun startSettings() = object : FragmentScreen {
-        override val screenKey: String = BACKSTACK_NAME_SETTINGS
-        override fun createFragment(factory: FragmentFactory): Fragment {
-            return SettingsFragment.newInstance()
-        }
+    fun startSettings() = BaseFragmentScreen(
+        screenKey = SCREEN_KEY_SETTINGS,
+        baskStackName = BACKSTACK_NAME_SETTINGS
+    ) {
+        SettingsFragment.newInstance()
     }
+
+    const val SCREEN_KEY_SETTINGS = "settings_screen"
 }
