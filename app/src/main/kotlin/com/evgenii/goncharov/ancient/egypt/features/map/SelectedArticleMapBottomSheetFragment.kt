555a5b54d7cb4c3ec12bf4cfd90c6d5017a68b25
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-06 19:04:05 +0300

    added binding to SelectedArticleMapBottomSheetFragment and layout

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/map/SelectedArticleMapBottomSheetFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/map/SelectedArticleMapBottomSheetFragment.kt
@@ -1,7 +1,24 @@
 package com.evgenii.goncharov.ancient.egypt.features.map
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.evgenii.goncharov.ancient.egypt.R
+import com.evgenii.goncharov.ancient.egypt.databinding.BottomSheetSelectedArticleBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 /** №7.2 */
-class SelectedArticleMapBottomSheetFragment : Fragment() {
+class SelectedArticleMapBottomSheetFragment : BottomSheetDialogFragment(
+    R.layout.bottom_sheet_selected_article
+) {
+
+    private val binding: BottomSheetSelectedArticleBinding by viewBinding(BottomSheetSelectedArticleBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.initUi()
+    }
+
+    private fun BottomSheetSelectedArticleBinding.initUi() {
+
+    }
 }
