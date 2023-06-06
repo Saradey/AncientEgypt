9214529c988a71a19db326b64b150cffb7222656
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-06 22:04:10 +0300

    added logic to ToolbarVisibilityManager

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/utils/ToolbarVisibilityManager.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/utils/ToolbarVisibilityManager.kt
@@ -1,13 +1,18 @@
 package com.evgenii.goncharov.ancient.egypt.features.main.utils
 
 import com.evgenii.goncharov.ancient.egypt.features.main.contracts.SetVisibilityToBottomMenuToolbarListener
-import com.github.terrakok.cicerone.androidx.FragmentScreen
+import com.evgenii.goncharov.ancient.egypt.features.main.navigation.MainBottomNavigator.Companion.BACKSTACK_NAME_ALL
+import com.evgenii.goncharov.ancient.egypt.features.main.navigation.MainBottomNavigator.Companion.BACKSTACK_NAME_MAIN
 
 class ToolbarVisibilityManager(
     private val setVisibilityToolbarListener: SetVisibilityToBottomMenuToolbarListener
 ) {
 
     fun visibilityToolbarChange(backStackName: String) {
-
+        when(backStackName) {
+            BACKSTACK_NAME_MAIN,
+            BACKSTACK_NAME_ALL -> setVisibilityToolbarListener.setVisibilityToolbar(true)
+            else -> setVisibilityToolbarListener.setVisibilityToolbar(false)
+        }
     }
 }
