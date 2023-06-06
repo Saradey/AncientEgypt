a17dcda6308847653aec502088a530c3d4904057
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-06 21:09:50 +0300

    added logic to ToolbarVisibilityManager

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/utils/ToolbarVisibilityManager.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/utils/ToolbarVisibilityManager.kt
@@ -1,6 +1,8 @@
 package com.evgenii.goncharov.ancient.egypt.features.main.utils
 
+import com.evgenii.goncharov.ancient.egypt.features.articles.navigation.AllArticlesScreen
 import com.evgenii.goncharov.ancient.egypt.features.main.contracts.SetVisibilityToBottomMenuToolbarListener
+import com.evgenii.goncharov.ancient.egypt.features.main.navigation.StartMainScreen
 import com.github.terrakok.cicerone.androidx.FragmentScreen
 
 class ToolbarVisibilityManager(
@@ -8,6 +10,14 @@ class ToolbarVisibilityManager(
 ) {
 
     fun visibilityToolbarChange(fragmentScreen: FragmentScreen) {
+        when (fragmentScreen) {
+            is AllArticlesScreen, is StartMainScreen -> {
+                setVisibilityToolbarListener.setVisibilityToolbar(true)
+            }
 
+            else -> {
+                setVisibilityToolbarListener.setVisibilityToolbar(false)
+            }
+        }
     }
 }
