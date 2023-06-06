69d3130ad07ad2a0cd78d2f68f5aac7ddd387f64
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-06 23:07:22 +0300

    refactor startSelectedCategory

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/articles/navigation/ArticlesScreens.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/articles/navigation/ArticlesScreens.kt
@@ -1,7 +1,5 @@
 package com.evgenii.goncharov.ancient.egypt.features.articles.navigation
 
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentFactory
 import com.evgenii.goncharov.ancient.egypt.base.BaseFragmentScreen
 import com.evgenii.goncharov.ancient.egypt.features.articles.AllArticlesFragment
 import com.evgenii.goncharov.ancient.egypt.features.articles.FavoriteArticlesFragment
@@ -9,7 +7,6 @@ import com.evgenii.goncharov.ancient.egypt.features.articles.SelectedCategoryFra
 import com.evgenii.goncharov.ancient.egypt.features.main.navigation.MainBottomNavigator.Companion.BACKSTACK_NAME_ALL
 import com.evgenii.goncharov.ancient.egypt.features.main.navigation.MainBottomNavigator.Companion.BACKSTACK_NAME_EVERYWHERE
 import com.evgenii.goncharov.ancient.egypt.features.main.navigation.MainBottomNavigator.Companion.BACKSTACK_NAME_FAVORITE
-import com.github.terrakok.cicerone.androidx.FragmentScreen
 
 object ArticlesScreens {
 
@@ -27,11 +24,11 @@ object ArticlesScreens {
         AllArticlesFragment.newInstance()
     }
 
-    fun startSelectedCategory() = object : FragmentScreen {
-        override val screenKey: String = BACKSTACK_NAME_EVERYWHERE
-        override fun createFragment(factory: FragmentFactory): Fragment {
-            return SelectedCategoryFragment.newInstance()
-        }
+    fun startSelectedCategory() = BaseFragmentScreen(
+        screenKey = SCREEN_KEY_SELECTED_CATEGORY,
+        baskStackName = BACKSTACK_NAME_EVERYWHERE
+    ) {
+        SelectedCategoryFragment.newInstance()
     }
 
     const val SCREEN_KEY_FAVORITE = "favorite_screen"
