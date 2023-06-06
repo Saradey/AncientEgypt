baf3f798161323da5e813d6583c68b20e68eff24
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-06 18:44:56 +0300

    inject router to SelectedArticleFragment

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/contents/SelectedArticleFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/contents/SelectedArticleFragment.kt
@@ -1,11 +1,39 @@
 package com.evgenii.goncharov.ancient.egypt.features.contents
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.evgenii.goncharov.ancient.egypt.R
+import com.evgenii.goncharov.ancient.egypt.databinding.FragmentSelectedArticleBinding
+import com.evgenii.goncharov.ancient.egypt.di.NavigationModule
+import com.github.terrakok.cicerone.Router
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import javax.inject.Named
 
 /** №6.1 */
+@AndroidEntryPoint
 class SelectedArticleFragment : Fragment(R.layout.fragment_selected_article) {
 
+    @Inject @Named(NavigationModule.QUALIFIER_ACTIVITY_NAVIGATION) lateinit var mainActivityRouter: Router
+    private val binding: FragmentSelectedArticleBinding by viewBinding(FragmentSelectedArticleBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.initUi()
+    }
+
+    private fun FragmentSelectedArticleBinding.initUi() {
+        btnNext1.setOnClickListener {
+
+        }
+        btnNext2.setOnClickListener {
+
+        }
+        btnNext3.setOnClickListener {
+
+        }
+    }
 
     companion object {
         fun newInstance() = SelectedArticleFragment()
