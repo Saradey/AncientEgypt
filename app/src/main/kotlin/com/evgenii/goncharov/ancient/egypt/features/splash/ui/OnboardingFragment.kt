08587184ffdd452aa0e454fe15254409f85bfb8c
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-07 15:03:36 +0300

    added OnboardingViewModel

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/splash/ui/OnboardingFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/splash/ui/OnboardingFragment.kt
@@ -3,15 +3,12 @@ package com.evgenii.goncharov.ancient.egypt.features.splash.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.evgenii.goncharov.ancient.egypt.R
 import com.evgenii.goncharov.ancient.egypt.databinding.FragmentOnboardingBinding
-import com.evgenii.goncharov.ancient.egypt.di.NavigationModule.QUALIFIER_ACTIVITY_NAVIGATION
-import com.evgenii.goncharov.ancient.egypt.features.main.navigation.MainScreens
-import com.github.terrakok.cicerone.Router
+import com.evgenii.goncharov.ancient.egypt.features.splash.view.models.OnboardingViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
-import javax.inject.Named
 
 /**
  * №1.2
@@ -20,12 +17,12 @@ import javax.inject.Named
 @AndroidEntryPoint
 class OnboardingFragment : Fragment(R.layout.fragment_onboarding) {
 
-    @Inject @Named(QUALIFIER_ACTIVITY_NAVIGATION) lateinit var mainActivityRouter: Router
+    private val viewModel: OnboardingViewModel by viewModels()
     private val binding: FragmentOnboardingBinding by viewBinding(FragmentOnboardingBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.btnNext.setOnClickListener {
-            mainActivityRouter.replaceScreen(MainScreens.startMainBottomMenu())
+            viewModel.goToTheMainBottomMenu()
         }
     }
 
---
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/splash/view/models/OnboardingViewModel.kt
@@ -0,0 +1,19 @@
+package com.evgenii.goncharov.ancient.egypt.features.splash.view.models
+
+import androidx.lifecycle.ViewModel
+import com.evgenii.goncharov.ancient.egypt.di.NavigationModule.QUALIFIER_ACTIVITY_NAVIGATION
+import com.evgenii.goncharov.ancient.egypt.features.main.navigation.MainScreens
+import com.github.terrakok.cicerone.Router
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import javax.inject.Named
+
+@HiltViewModel
+class OnboardingViewModel @Inject constructor(
+    @Named(QUALIFIER_ACTIVITY_NAVIGATION) private var activityRouter: Router
+) : ViewModel() {
+
+    fun goToTheMainBottomMenu() {
+        activityRouter.replaceScreen(MainScreens.startMainBottomMenu())
+    }
+}
