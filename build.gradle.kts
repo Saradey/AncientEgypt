8b251295f8503b583c888e1e6397950ca4026d77
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-07 12:35:19 +0300

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KaptGenerateStubs
 
 plugins {
-    id("com.android.application") version "8.0.1" apply false
-    id("com.android.library") version "8.0.1" apply false
+    id("com.android.application") version "8.0.2" apply false
+    id("com.android.library") version "8.0.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.20" apply false
     id("com.google.dagger.hilt.android") version "2.46.1" apply false
 }
