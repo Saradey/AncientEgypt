b0505252478708a7e675945b515ea714406e1739
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-07 13:14:28 +0300

    refactor contents

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/contents/navigation/ContentScreens.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/contents/navigation/ContentScreens.kt
@@ -1,17 +1,13 @@
 package com.evgenii.goncharov.ancient.egypt.features.contents.navigation
 
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentFactory
 import com.evgenii.goncharov.ancient.egypt.features.contents.SelectedArticleFragment
 import com.evgenii.goncharov.ancient.egypt.features.contents.SelectedPhotoFragment
 import com.github.terrakok.cicerone.androidx.FragmentScreen
 
 object ContentScreens {
 
-    fun startSelectedArticle() = object : FragmentScreen {
-        override fun createFragment(factory: FragmentFactory): Fragment {
-            return SelectedArticleFragment.newInstance()
-        }
+    fun startSelectedArticle() = FragmentScreen { _ ->
+        SelectedArticleFragment.newInstance()
     }
 
     fun startSelectedPhoto() = FragmentScreen { _ ->
