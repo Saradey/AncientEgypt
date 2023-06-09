e84c0be44a71921bdfbeec4c51d8130d5b88f1e3
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-09 20:04:10 +0300

    added logic OnboardingRepositoryImpl

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/repositories/impl/OnboardingRepositoryImpl.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/repositories/impl/OnboardingRepositoryImpl.kt
@@ -1,15 +1,22 @@
 package com.evgenii.goncharov.ancient.egypt.repositories.impl
 
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
 import com.evgenii.goncharov.ancient.egypt.repositories.OnboardingRepository
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
-class OnboardingRepositoryImpl @Inject constructor() : OnboardingRepository {
+class OnboardingRepositoryImpl @Inject constructor(
+    @ApplicationContext private val appContext: Context
+) : OnboardingRepository {
 
     override fun isFirstStartOnboarding(): Boolean {
-        return false
+        val pref = appContext.getSharedPreferences(PREF_ONBOARDING_INFO, MODE_PRIVATE)
+        return pref.getBoolean(KEY_IS_FIRST_START_ONBOARDING, true)
     }
 
     companion object {
-
+        private const val PREF_ONBOARDING_INFO = "PREF_ONBOARDING_INFO"
+        private const val KEY_IS_FIRST_START_ONBOARDING = "KEY_IS_FIRST_START_ONBOARDING"
     }
 }
