91048c9d74d11f05a0becd8749dc58decd58d5a4
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-09 18:27:34 +0300

    added FavoriteArticlesViewModel

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/articles/ui/FavoriteArticlesFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/articles/ui/FavoriteArticlesFragment.kt
@@ -3,17 +3,12 @@ package com.evgenii.goncharov.ancient.egypt.features.articles.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.evgenii.goncharov.ancient.egypt.R
 import com.evgenii.goncharov.ancient.egypt.databinding.FragmentFavoriteArticlesBinding
-import com.evgenii.goncharov.ancient.egypt.di.NavigationModule.QUALIFIER_ACTIVITY_NAVIGATION
-import com.evgenii.goncharov.ancient.egypt.di.NavigationModule.QUALIFIER_BOTTOM_MENU_NAVIGATION
-import com.evgenii.goncharov.ancient.egypt.features.articles.navigation.ArticlesScreens
-import com.evgenii.goncharov.ancient.egypt.features.content.navigation.ContentScreens
-import com.github.terrakok.cicerone.Router
+import com.evgenii.goncharov.ancient.egypt.features.articles.view.models.FavoriteArticlesViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
-import javax.inject.Named
 
 /**
  * №4.2
@@ -22,11 +17,8 @@ import javax.inject.Named
 @AndroidEntryPoint
 class FavoriteArticlesFragment : Fragment(R.layout.fragment_favorite_articles) {
 
-    @Inject @Named(QUALIFIER_BOTTOM_MENU_NAVIGATION) lateinit var router: Router
-    @Inject @Named(QUALIFIER_ACTIVITY_NAVIGATION) lateinit var mainActivityRouter: Router
-    private val binding: FragmentFavoriteArticlesBinding by viewBinding(
-        FragmentFavoriteArticlesBinding::bind
-    )
+    private val viewModel: FavoriteArticlesViewModel by viewModels()
+    private val binding: FragmentFavoriteArticlesBinding by viewBinding(FragmentFavoriteArticlesBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.initUi()
@@ -35,10 +27,10 @@ class FavoriteArticlesFragment : Fragment(R.layout.fragment_favorite_articles) {
 
     private fun FragmentFavoriteArticlesBinding.initUi() {
         btnNext1.setOnClickListener {
-            router.navigateTo(ArticlesScreens.startSelectedCategory())
+            viewModel.goToTheSelectedCategory()
         }
         btnNext2.setOnClickListener {
-            mainActivityRouter.navigateTo(ContentScreens.startSelectedArticle())
+            viewModel.goToTheSelectedArticle()
         }
     }
 
