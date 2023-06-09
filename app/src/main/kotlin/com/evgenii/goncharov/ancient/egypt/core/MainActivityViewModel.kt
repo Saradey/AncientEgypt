d5de317b721ef53e327cdeac44f837903435e860
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-09 19:57:47 +0300

    refactor

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/core/MainActivityViewModel.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/core/MainActivityViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModel
 import com.evgenii.goncharov.ancient.egypt.di.NavigationModule.QUALIFIER_ACTIVITY_NAVIGATION
 import com.evgenii.goncharov.ancient.egypt.features.main.navigation.MainScreens
 import com.evgenii.goncharov.ancient.egypt.features.splash.navigation.SplashScreens
-import com.evgenii.goncharov.ancient.egypt.use.cases.OnboardingUseCase
+import com.evgenii.goncharov.ancient.egypt.repositories.OnboardingRepository
 import com.github.terrakok.cicerone.Router
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -14,26 +14,22 @@ import javax.inject.Named
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(
     @Named(QUALIFIER_ACTIVITY_NAVIGATION) private val activityRouter: Router,
-    private val onboardingUseCase: OnboardingUseCase
+    private val onboardingRepository: OnboardingRepository
 ) : ViewModel() {
 
     fun startFirstScreen() {
-//        when {
-//            isStartOnboarding() -> activityRouter.navigateTo(SplashScreens.startOnboarding())
-//        }
-//
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-//            if (onboardingRepository.isFirstStartOnboarding()) {
-//                activityRouter.navigateTo(MainScreens.startMainBottomMenu())
-//            } else {
-//                activityRouter.navigateTo(SplashScreens.startOnboarding())
-//            }
-//        } else {
-//            activityRouter.navigateTo(SplashScreens.startSplash())
-//        }
+        when {
+            isStartOnboarding() -> activityRouter.navigateTo(SplashScreens.startOnboarding())
+            isStartBottomMenu() -> activityRouter.navigateTo(MainScreens.startMainBottomMenu())
+            else -> activityRouter.navigateTo(SplashScreens.startSplash())
+        }
     }
 
-//    private fun isStartOnboarding(): Boolean {
-//        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && onboardingRepository.isFirstStartOnboarding()
-//    }
+    private fun isStartOnboarding(): Boolean {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && onboardingRepository.isFirstStartOnboarding()
+    }
+
+    private fun isStartBottomMenu(): Boolean {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && !onboardingRepository.isFirstStartOnboarding()
+    }
 }
--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/use/cases/OnboardingUseCase.kt
+++
@@ -1,4 +0,0 @@
-package com.evgenii.goncharov.ancient.egypt.use.cases
-
-interface OnboardingUseCase {
-}
