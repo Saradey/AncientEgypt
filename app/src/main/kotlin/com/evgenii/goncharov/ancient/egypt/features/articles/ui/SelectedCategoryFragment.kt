e16ec0ae2f693edc6db604f99b902203c633c1ff
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-09 18:44:20 +0300

    added SelectedCategoryViewModel

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/articles/ui/SelectedCategoryFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/articles/ui/SelectedCategoryFragment.kt
@@ -3,15 +3,12 @@ package com.evgenii.goncharov.ancient.egypt.features.articles.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.evgenii.goncharov.ancient.egypt.R
 import com.evgenii.goncharov.ancient.egypt.databinding.FragmentSelectedCategoryBinding
-import com.evgenii.goncharov.ancient.egypt.di.NavigationModule.QUALIFIER_ACTIVITY_NAVIGATION
-import com.evgenii.goncharov.ancient.egypt.features.content.navigation.ContentScreens
-import com.github.terrakok.cicerone.Router
+import com.evgenii.goncharov.ancient.egypt.features.articles.view.models.SelectedCategoryViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
-import javax.inject.Named
 
 /**
  * №4.3
@@ -20,7 +17,7 @@ import javax.inject.Named
 @AndroidEntryPoint
 class SelectedCategoryFragment : Fragment(R.layout.fragment_selected_category) {
 
-    @Inject @Named(QUALIFIER_ACTIVITY_NAVIGATION) lateinit var mainActivityRouter: Router
+    private val viewModel: SelectedCategoryViewModel by viewModels()
     private val binding: FragmentSelectedCategoryBinding by viewBinding(FragmentSelectedCategoryBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -30,7 +27,7 @@ class SelectedCategoryFragment : Fragment(R.layout.fragment_selected_category) {
 
     private fun FragmentSelectedCategoryBinding.initUi() {
         btnNext1.setOnClickListener {
-            mainActivityRouter.navigateTo(ContentScreens.startSelectedArticle())
+            viewModel.goToTheSelectedArticle()
         }
     }
 
---
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/articles/view/models/SelectedCategoryViewModel.kt
@@ -0,0 +1,19 @@
+package com.evgenii.goncharov.ancient.egypt.features.articles.view.models
+
+import androidx.lifecycle.ViewModel
+import com.evgenii.goncharov.ancient.egypt.di.NavigationModule.QUALIFIER_ACTIVITY_NAVIGATION
+import com.evgenii.goncharov.ancient.egypt.features.content.navigation.ContentScreens
+import com.github.terrakok.cicerone.Router
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import javax.inject.Named
+
+@HiltViewModel
+class SelectedCategoryViewModel @Inject constructor(
+    @Named(QUALIFIER_ACTIVITY_NAVIGATION) private val activityRouter: Router
+) : ViewModel() {
+
+    fun goToTheSelectedArticle() {
+        activityRouter.navigateTo(ContentScreens.startSelectedArticle())
+    }
+}
