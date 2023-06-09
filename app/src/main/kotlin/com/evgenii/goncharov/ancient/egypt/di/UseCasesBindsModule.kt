e12fa856fdb58cda8ca0dfd43923317660c259ff
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-09 19:42:06 +0300

    added OnboardingUseCase to MainActivityViewModel

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/di/UseCasesBindsModule.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/di/UseCasesBindsModule.kt
@@ -2,13 +2,17 @@ package com.evgenii.goncharov.ancient.egypt.di
 
 import com.evgenii.goncharov.ancient.egypt.use.cases.OnboardingUseCase
 import com.evgenii.goncharov.ancient.egypt.use.cases.impl.OnboardingUseCaseImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 interface UseCasesBindsModule {
 
+    @Binds
+    @Singleton
     fun OnboardingUseCaseImpl.bindOnboardingUseCase(): OnboardingUseCase
 }
