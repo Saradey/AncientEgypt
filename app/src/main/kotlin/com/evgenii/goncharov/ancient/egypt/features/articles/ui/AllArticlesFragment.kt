ecaea9b1ac016bd8f1c394c94928f6655d485241
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-09 18:19:59 +0300

    added AllArticlesViewModel

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/articles/ui/AllArticlesFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/articles/ui/AllArticlesFragment.kt
@@ -3,17 +3,12 @@ package com.evgenii.goncharov.ancient.egypt.features.articles.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.evgenii.goncharov.ancient.egypt.R
 import com.evgenii.goncharov.ancient.egypt.databinding.FragmentAllArticlesBinding
-import com.evgenii.goncharov.ancient.egypt.di.NavigationModule.QUALIFIER_ACTIVITY_NAVIGATION
-import com.evgenii.goncharov.ancient.egypt.di.NavigationModule.QUALIFIER_BOTTOM_MENU_NAVIGATION
-import com.evgenii.goncharov.ancient.egypt.features.articles.navigation.ArticlesScreens
-import com.evgenii.goncharov.ancient.egypt.features.content.navigation.ContentScreens
-import com.github.terrakok.cicerone.Router
+import com.evgenii.goncharov.ancient.egypt.features.articles.view.models.AllArticlesViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
-import javax.inject.Named
 
 /**
  * №4.1
@@ -22,8 +17,7 @@ import javax.inject.Named
 @AndroidEntryPoint
 class AllArticlesFragment : Fragment(R.layout.fragment_all_articles) {
 
-    @Inject @Named(QUALIFIER_BOTTOM_MENU_NAVIGATION) lateinit var router: Router
-    @Inject @Named(QUALIFIER_ACTIVITY_NAVIGATION) lateinit var mainActivityRouter: Router
+    private val viewModel: AllArticlesViewModel by viewModels()
     private val binding: FragmentAllArticlesBinding by viewBinding(FragmentAllArticlesBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -33,10 +27,10 @@ class AllArticlesFragment : Fragment(R.layout.fragment_all_articles) {
 
     private fun FragmentAllArticlesBinding.initUi() {
         btnNext1.setOnClickListener {
-            router.navigateTo(ArticlesScreens.startSelectedCategory())
+            viewModel.goToTheSelectedCategory()
         }
         btnNext2.setOnClickListener {
-            mainActivityRouter.navigateTo(ContentScreens.startSelectedArticle())
+            viewModel.goToTheSelectedArticle()
         }
     }
 
