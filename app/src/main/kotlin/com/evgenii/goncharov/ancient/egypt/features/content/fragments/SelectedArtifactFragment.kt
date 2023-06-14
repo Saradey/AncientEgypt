2ebadc856cfbc88c65c22e9c6fff24776b780bfc
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-14 16:24:39 +0300

    added navigation to SelectedArtifactFragment

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/content/fragments/SelectedArtifactFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/content/fragments/SelectedArtifactFragment.kt
@@ -3,16 +3,21 @@ package com.evgenii.goncharov.ancient.egypt.features.content.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.evgenii.goncharov.ancient.egypt.R
 import com.evgenii.goncharov.ancient.egypt.databinding.FragmentSelectedArtifactBinding
+import com.evgenii.goncharov.ancient.egypt.features.content.view.models.SelectedArtifactViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * № 8.1
  * Doc: https://docs.google.com/document/d/1iyv35Iw84vkFnAeFNEgGS1w-tyw09L6CQMeyear_4bk/edit
  */
+@AndroidEntryPoint
 class SelectedArtifactFragment : Fragment(R.layout.fragment_selected_artifact) {
 
+    private val viewModel: SelectedArtifactViewModel by viewModels()
     private val binding: FragmentSelectedArtifactBinding by viewBinding(FragmentSelectedArtifactBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -20,6 +25,9 @@ class SelectedArtifactFragment : Fragment(R.layout.fragment_selected_artifact) {
     }
 
     private fun FragmentSelectedArtifactBinding.initUi() {
+        btnNext1.setOnClickListener {
+            viewModel.goToTheSelectedArticle()
+        }
     }
 
     companion object {
