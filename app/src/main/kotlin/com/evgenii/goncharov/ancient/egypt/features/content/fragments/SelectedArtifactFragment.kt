c0e77bff4ad64f2ac3f1281a25e48a7a0bfa33a4
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-14 16:18:43 +0300

    added test widgets

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/content/fragments/SelectedArtifactFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/content/fragments/SelectedArtifactFragment.kt
@@ -7,6 +7,10 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.evgenii.goncharov.ancient.egypt.R
 import com.evgenii.goncharov.ancient.egypt.databinding.FragmentSelectedArtifactBinding
 
+/**
+ * № 8.1
+ * Doc: https://docs.google.com/document/d/1iyv35Iw84vkFnAeFNEgGS1w-tyw09L6CQMeyear_4bk/edit
+ */
 class SelectedArtifactFragment : Fragment(R.layout.fragment_selected_artifact) {
 
     private val binding: FragmentSelectedArtifactBinding by viewBinding(FragmentSelectedArtifactBinding::bind)
