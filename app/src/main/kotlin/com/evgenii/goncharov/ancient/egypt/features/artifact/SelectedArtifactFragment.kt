2c8af8167effbe89ed43f628d4eb57c7c1bfd292
Evgeny Goncharov <jtgn@yandex.ru>
2023-06-14 13:59:26 +0300

    added fragment_selected_artifact

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/artifact/SelectedArtifactFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/artifact/SelectedArtifactFragment.kt
@@ -1,6 +1,20 @@
 package com.evgenii.goncharov.ancient.egypt.features.artifact
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.evgenii.goncharov.ancient.egypt.R
+import com.evgenii.goncharov.ancient.egypt.databinding.FragmentSelectedArtifactBinding
 
-class SelectedArtifactFragment : Fragment() {
+class SelectedArtifactFragment : Fragment(R.layout.fragment_selected_artifact) {
+
+    private val binding: FragmentSelectedArtifactBinding by viewBinding(FragmentSelectedArtifactBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.initUi()
+    }
+
+    private fun FragmentSelectedArtifactBinding.initUi() {
+    }
 }
