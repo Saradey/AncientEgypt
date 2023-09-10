478dfa5b6cc9de0cc718a06c77ef915ef314b360
Evgeny Goncharov <jtgn@yandex.ru>
2023-09-10 14:52:44 +0300

    added initViewPager

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/splash/fragments/OnboardingFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/splash/fragments/OnboardingFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.viewpager2.widget.ViewPager2
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.evgenii.goncharov.ancient.egypt.R
 import com.evgenii.goncharov.ancient.egypt.databinding.FragmentOnboardingBinding
@@ -34,6 +35,30 @@ class OnboardingFragment : Fragment(R.layout.fragment_onboarding) {
     }
 
     private fun FragmentOnboardingBinding.initUi() {
+        initViewPager()
+    }
+
+    private fun FragmentOnboardingBinding.initViewPager() {
+        vp2Content.adapter = adapter
+        vp2Content.registerOnPageChangeCallback(
+            object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageScrolled(
+                    position: Int,
+                    positionOffset: Float,
+                    positionOffsetPixels: Int
+                ) {
+                    if (nextDotsIndicator != position) {
+                        nextDotsIndicator = position
+                        dtiIndicator.nextPage = nextDotsIndicator + 1
+                    }
+                    if (position >= 2) {
+                        txvNext.setText(R.string.get_started)
+                    } else {
+                        txvNext.setText(R.string.next)
+                    }
+                }
+            }
+        )
     }
 
     companion object {
