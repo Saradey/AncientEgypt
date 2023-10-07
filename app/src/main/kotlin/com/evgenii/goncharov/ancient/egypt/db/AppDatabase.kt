c742f5e31299f634c72358b43ab5959d959157d9
Evgeny Goncharov <jtgn@yandex.ru>
2023-10-07 19:30:16 +0300

    task-1 added empty data base

---
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/db/AppDatabase.kt
@@ -0,0 +1,12 @@
+package com.evgenii.goncharov.ancient.egypt.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [],
+    version = 1
+)
+abstract class AppDatabase: RoomDatabase() {
+
+}
--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/di/CommonModule.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/di/CommonModule.kt
@@ -1,17 +1,34 @@
 package com.evgenii.goncharov.ancient.egypt.di
 
+import android.content.Context
 import android.os.Build
+import androidx.room.Room
+import com.evgenii.goncharov.ancient.egypt.db.AppDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class CommonModule {
 
     @Provides
-    fun provideBuildVersion() : Int {
+    fun provideBuildVersion(): Int {
         return Build.VERSION.SDK_INT
+    }
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(
+        @ApplicationContext context: Context
+    ): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "ancient_egypt_db"
+        ).build()
     }
 }
