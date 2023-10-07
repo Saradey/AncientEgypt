e25691af8af2909b430419d04dff52e92a311d7f
Evgeny Goncharov <jtgn@yandex.ru>
2023-10-07 19:14:12 +0300

    task-1 fix crash

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/fragments/MainFragment.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/fragments/MainFragment.kt
@@ -7,7 +7,9 @@ import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.evgenii.goncharov.ancient.egypt.R
 import com.evgenii.goncharov.ancient.egypt.databinding.FragmentMainBinding
+import com.evgenii.goncharov.ancient.egypt.features.main.models.entities.BaseContent
 import com.evgenii.goncharov.ancient.egypt.features.main.models.state.MainContentUiState
+import com.evgenii.goncharov.ancient.egypt.features.main.ui.MainContentAdapter
 import com.evgenii.goncharov.ancient.egypt.features.main.view.models.MainViewModel
 import com.evgenii.goncharov.ancient.egypt.utils.StatusBarUtils
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,10 +19,11 @@ import dagger.hilt.android.AndroidEntryPoint
  * Doc: https://docs.google.com/document/d/1R1-U1C_VLGf1kwnR2W7SJf_i0znYaJqmfNhZv0G8T5E/edit
  * */
 @AndroidEntryPoint
-class MainFragment : Fragment(R.layout.fragment_main_old) {
+class MainFragment : Fragment(R.layout.fragment_main) {
 
     private val viewModel: MainViewModel by viewModels()
     private val binding: FragmentMainBinding by viewBinding(FragmentMainBinding::bind)
+    private val adapter: MainContentAdapter = MainContentAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,10 +44,14 @@ class MainFragment : Fragment(R.layout.fragment_main_old) {
         when (contentUiState) {
             MainContentUiState.Loading -> {}
             MainContentUiState.LoadingUpdate -> {}
-            is MainContentUiState.Content -> {}
+            is MainContentUiState.Content -> setContent(contentUiState.content)
             is MainContentUiState.Error -> {}
             is MainContentUiState.ErrorUpdate -> {}
         }
+    }
+
+    private fun setContent(content: List<BaseContent>) {
+        adapter.items = content
     }
 
     private fun FragmentMainBinding.initUi() {
