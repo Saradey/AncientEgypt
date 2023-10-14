445227bcd2fec02c61d74a039fa9e54ca596e5fd
Evgeny Goncharov <jtgn@yandex.ru>
2023-10-14 16:38:46 +0300

    task-2 added stories shimmer

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/ui/costume/view/StoriesShimmerView.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/ui/costume/view/StoriesShimmerView.kt
@@ -3,6 +3,7 @@ package com.evgenii.goncharov.ancient.egypt.features.main.ui.costume.view
 import android.content.Context
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.evgenii.goncharov.ancient.egypt.R
 
 class StoriesShimmerView @JvmOverloads constructor(
     context: Context,
@@ -11,6 +12,6 @@ class StoriesShimmerView @JvmOverloads constructor(
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
     init {
-
+        inflate(context, R.layout.view_stories_shimmer, this)
     }
 }
