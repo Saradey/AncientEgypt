476ac53b58e837592ad243a5f684aef363a76983
Evgeny Goncharov <jtgn@yandex.ru>
2023-10-22 16:28:27 +0300

    task-3 added logic to StoriesDatabaseUseCase

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/use/cases/StoriesDatabaseUseCase.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/use/cases/StoriesDatabaseUseCase.kt
@@ -1,4 +1,9 @@
 package com.evgenii.goncharov.ancient.egypt.features.main.use.cases
 
+import com.evgenii.goncharov.ancient.egypt.base.models.model.BaseModel
+import com.evgenii.goncharov.ancient.egypt.features.main.models.models.StoriesModel
+
 interface StoriesDatabaseUseCase {
+
+    suspend operator fun invoke(storiesId: String) : BaseModel<StoriesModel>
 }
--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/use/cases/impl/StoriesDatabaseUseCaseImpl.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/use/cases/impl/StoriesDatabaseUseCaseImpl.kt
@@ -1,5 +1,7 @@
 package com.evgenii.goncharov.ancient.egypt.features.main.use.cases.impl
 
+import com.evgenii.goncharov.ancient.egypt.base.models.model.BaseModel
+import com.evgenii.goncharov.ancient.egypt.features.main.models.models.StoriesModel
 import com.evgenii.goncharov.ancient.egypt.features.main.repositories.StoriesDatabaseRepository
 import com.evgenii.goncharov.ancient.egypt.features.main.use.cases.StoriesDatabaseUseCase
 import javax.inject.Inject
@@ -8,4 +10,7 @@ class StoriesDatabaseUseCaseImpl @Inject constructor(
     private val storiesDatabaseRepository: StoriesDatabaseRepository
 ) : StoriesDatabaseUseCase {
 
+    override suspend fun invoke(storiesId: String): BaseModel<StoriesModel> {
+        return storiesDatabaseRepository(storiesId)
+    }
 }
