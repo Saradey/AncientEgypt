680040373e45732949f58c1edad0549ebcd804fc
Evgeny Goncharov <jtgn@yandex.ru>
2023-10-22 18:06:08 +0300

    task-3 added logic to initStoriesState

--- app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/view/models/StoriesViewModel.kt
+++ app/src/main/kotlin/com/evgenii/goncharov/ancient/egypt/features/main/view/models/StoriesViewModel.kt
@@ -1,10 +1,12 @@
 package com.evgenii.goncharov.ancient.egypt.features.main.view.models
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.evgenii.goncharov.ancient.egypt.base.models.state.UiState
 import com.evgenii.goncharov.ancient.egypt.di.NavigationModule.QUALIFIER_ACTIVITY_NAVIGATION
 import com.evgenii.goncharov.ancient.egypt.features.articles.navigation.ArticlesScreens
 import com.evgenii.goncharov.ancient.egypt.features.content.navigation.ContentScreens
+import com.evgenii.goncharov.ancient.egypt.features.main.models.dto.request.StoriesModelRequest
 import com.evgenii.goncharov.ancient.egypt.features.main.models.models.SelectedStoriesModel
 import com.evgenii.goncharov.ancient.egypt.features.main.models.models.StoriesModel
 import com.evgenii.goncharov.ancient.egypt.features.main.use.cases.StoriesDatabaseUseCase
@@ -12,8 +14,10 @@ import com.evgenii.goncharov.ancient.egypt.features.main.use.cases.StoriesNetwor
 import com.evgenii.goncharov.ancient.egypt.features.map.navigation.MapScreens
 import com.github.terrakok.cicerone.Router
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -29,13 +33,27 @@ class StoriesViewModel @Inject constructor(
     private var currentStories: String = ""
     private var allStories: MutableList<String> = mutableListOf()
 
-    fun initStoriesState() {
-
-    }
-
     fun initModel(model: SelectedStoriesModel) {
         currentStories = model.selectedStoriesId
         allStories.addAll(model.allStoriesId)
+    }
+
+    fun initStoriesState() {
+        viewModelScope.launch(CoroutineExceptionHandler { _, _ ->
+            _storiesState.value = UiState.Failure
+        }) {
+            val modelDatabase = storiesDatabaseUseCase(currentStories)
+            modelDatabase.data?.let { model ->
+                _storiesState.emit(UiState.Success(model))
+            } ?: run {
+                _storiesState.emit(UiState.Loading)
+                loadStoriesFromNetwork()
+            }
+        }
+    }
+
+    private suspend fun loadStoriesFromNetwork() {
+        val networkResponse = storiesNetworkUseCase(StoriesModelRequest(storiesId = currentStories))
     }
 
     fun goToTheSelectedArticle() {
